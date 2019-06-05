a1559a9a59f0d671ed538c8d738d72d0f112e39e
rvsanten <richard@maasoft.com>
2019-06-05 21:11:52 +0200

    Add files via upload

---
+++ receive.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+var amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://192.168.1.130', function(error0, connection) {
+  if (error0) {
+    throw error0;
+  }
+  connection.createChannel(function(error1, channel) {
+    if (error1) {
+      throw error1;
+    }
+    var queue = 'thingy_data';
+
+    channel.assertQueue(queue, {
+      durable: false
+    });
+
+    console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+channel.consume(queue, function(msg) {
+  console.log(" [x] Received %s", msg.content.toString());
+}, {
+    noAck: true
+  });
+  });
+});
+
---
+++ send.js
@@ -0,0 +1,96 @@
+#!/usr/bin/env node
+var Thingy = require('thingy52');
+var amqp = require('amqplib');
+var thingName = "";
+console.log('Button and Environment!');
+
+function onButtonChange(state) {
+    sendData(state);
+}
+
+function sendData(data) {
+   amqp.connect('amqp://192.168.1.130').then(function(conn) {
+    return conn.createChannel().then(function(ch) {
+    var q = 'thingy_data';
+    var ok = ch.assertQueue(q, {durable: false});
+
+    return ok.then(function(_qok) {
+      ch.sendToQueue(q, Buffer.from(data));
+      console.log("Sent '%s'", data);
+      return ch.close();
+    });
+  }).finally(function() { conn.close(); });
+}).catch(console.warn);
+}
+
+function onTemperatureData(temperature) {
+    sendData('Thingy: ' + thingName + ' Temperature: ' + temperature);
+}
+
+function onPressureData(pressure) {
+    sendData('Thingy: ' + thingName + ' Pressure sensor: ' + pressure);
+}
+
+function onHumidityData(humidity) {
+    sendData('Thingy: ' + thingName + ' Humidity sensor: ' + humidity);
+}
+
+function onGasData(gas) {
+    sendData('Thingy: ' + thingName + ' Gas sensor: eCO2 ' + gas.eco2 + ' - TVOC ' + gas.tvoc );
+}
+
+function onDiscover(thingy) {
+  thingName = thingy;
+  console.log('Discovered: ' + thingName);
+
+  thingy.on('disconnect', function() {
+    console.log('Disconnected!');
+  });
+
+  thingy.connectAndSetUp(function(error) {
+    console.log('Connected! ' + error);
+    thingy.on('buttonNotif', onButtonChange);
+    thingy.on('temperatureNotif', onTemperatureData);
+    thingy.on('pressureNotif', onPressureData);
+    thingy.on('humidityNotif', onHumidityData);
+    thingy.on('gasNotif', onGasData);
+
+    thingy.temperature_interval_set(10000, function(error) {
+        if (error) {
+            console.log('Temperature sensor configure! ' + error);
+        }
+    });
+    thingy.pressure_interval_set(10000, function(error) {
+        if (error) {
+            console.log('Pressure sensor configure! ' + error);
+        }
+    });
+    thingy.humidity_interval_set(10000, function(error) {
+        if (error) {
+            console.log('Humidity sensor configure! ' + error);
+        }
+    });
+    thingy.gas_mode_set(2, function(error) {
+        if (error) {
+            console.log('Gas sensor configure! ' + error);
+        }
+    });
+    thingy.button_enable(function(error) {
+      console.log('Button enabled! ' + error);
+    });
+    thingy.temperature_enable(function(error) {
+      console.log('Temperature sensor started! ' + ((error) ? error : ''));
+    });
+    thingy.pressure_enable(function(error) {
+        console.log('Pressure sensor started! ' + ((error) ? error : ''));
+    });
+    thingy.humidity_enable(function(error) {
+        console.log('Humidity sensor started! ' + ((error) ? error : ''));
+    });
+    thingy.gas_enable(function(error) {
+        console.log('Gas sensor started! ' + ((error) ? error : ''));
+    });
+  });
+}
+
+Thingy.discover(onDiscover);
