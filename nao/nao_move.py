f576be86b1687103918a6662c5f1934a06d903a2
Richard van Santen <richard@maasoft.com>
2019-08-21 15:54:20 +0200

    Add nao python code and docker image

---
+++ nao/nao_move.py
@@ -0,0 +1,17 @@
+import time
+from naoqi import ALProxy
+motionProxy = ALProxy("ALMotion", "192.168.1.13", 9559)
+motionProxy.setStiffnesses("Head", 1.0)
+names = "HeadPitch"
+fractionMaxSpeed = 0.8
+angles = 0.95
+motionProxy.setAngles(names,angles,fractionMaxSpeed)
+time.sleep(1.0)
+angles = 0.0
+motionProxy.setAngles(names,angles,fractionMaxSpeed)
+time.sleep(1.0)
+angles = 0.95
+motionProxy.setAngles(names,angles,fractionMaxSpeed)
+time.sleep(1.0)
+angles = 0.0
+motionProxy.setAngles(names,angles,fractionMaxSpeed)
